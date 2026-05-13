ffa066b571849e1d01aa770e287c912535009865
nandhakumar02042004 <nandhakumar01435@gmail.com>
2026-05-13 15:45:13 +0530

    style: reduce alumni hero height for a more compact design

--- src/components/Alumni/AlumniHero.jsx
+++ src/components/Alumni/AlumniHero.jsx
@@ -4,7 +4,7 @@ import alumniTech from '../../assets/alumni/alu.png'
 
 const AlumniHero = () => {
   return (
-    <section className="relative w-full h-[350px] md:h-[400px] lg:h-[500px] overflow-hidden">
+    <section className="relative w-full h-[280px] md:h-[350px] lg:h-[400px] overflow-hidden">
       <motion.img 
         initial={{ opacity: 0, scale: 1.05 }}
         animate={{ opacity: 1, scale: 1 }}
