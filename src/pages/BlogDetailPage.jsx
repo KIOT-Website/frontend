b1b72572afa3c0c7288afbe9124adfdf37af9c5d
nandhakumar02042004 <nandhakumar01435@gmail.com>
2026-07-07 14:39:04 +0530

    Reduce blog detail page title font size

--- src/pages/BlogDetailPage.jsx
+++ src/pages/BlogDetailPage.jsx
@@ -144,7 +144,7 @@ const BlogDetailPage = () => {
                     <span className="text-[10px] font-black uppercase tracking-widest">{blog.publish_date}</span>
                 </div>
 
-                <h1 className="text-4xl md:text-5xl lg:text-6xl font-black text-black mb-0 leading-[1.1] tracking-tight">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-black text-black mb-0 leading-[1.15] tracking-tight">
                     {blog.title}
                 </h1>
             </div>
